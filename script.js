0fa8aba9e8f8ba674c07938f5e560cfdce763071
Alejandro2010blue <dsalazaralvarez453@gmail.com>
2026-02-03 13:05:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Buscamos el botón y la caja en el documento
+const boton = document.getElementById('miBoton');
+const caja = document.querySelector('.contenedor-principal');
 
+// Qué pasa cuando hacemos clic
+boton.addEventListener('click', () => {
+    // Cambiamos colores dinámicamente
+    caja.style.borderColor = "#fccf05";
+    caja.style.transform = "translateY(-10px)"; // Un pequeño salto
+    
+    // Cambiamos el texto
+    boton.innerText = "¡PROYECTO ACTIVADO!";
+    boton.style.boxShadow = "0 0 30px #fccf05";
+
+    // Alerta de éxito
+    alert('¡Impresionante, Diego! Has creado una web profesional separando HTML, CSS y JS. 🚀');
+});
